494aa89d07d59218accf400a8806237fa4a32021
uzeyirbulentsahin <uzeyir.bulent.sahin@gmail.com>
2021-03-10 21:59:58 +0300

    ikinci ders 1 bölüm

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -7,44 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  product1 = {
-    productID: 1,
-    productName: 'Bardak',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product2 = {
-    productID: 2,
-    productName: 'Laptop',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product3 = {
-    productID: 3,
-    productName: 'Mouse',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product4 = {
-    productID: 4,
-    productName: 'Keyboard',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product5 = {
-    productID: 5,
-    productName: 'Camera',
-    categoryId: 1,
-    unitPrice: 5,
-  };
 
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5
-  ];
+  products:Product[] = [];
   constructor() { }
 
   ngOnInit(): void {
