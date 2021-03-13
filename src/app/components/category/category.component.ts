b0f1aa0cf2bad3e61e3d1ae41021bf010cbb77a8
uzeyirbulentsahin <uzeyir.bulent.sahin@gmail.com>
2021-03-13 22:42:47 +0300

    route geliyor

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -10,21 +10,29 @@ import { CategoryService } from 'src/app/services/category.service';
 export class CategoryComponent implements OnInit {
 
   categories:Category[] = [];
+  currentCategory:Category;
   dataLoaded=false;
   constructor(private categoryService:CategoryService) { }
-
   ngOnInit(): void {
     this.getCategories();
   }
-
   getCategories(){
     this.categoryService.getCategories().subscribe(response=> {
       this.categories=response.data
       this.dataLoaded=true;
     })
-    
-    
+  }
+  setCurrentCategory(category:Category){
+    this.currentCategory=category;
   }
 
-
+  getCurrentCategoryClass(category:Category){
+    if(category==this.currentCategory)
+    {
+      return  "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+   
+  }
 }
