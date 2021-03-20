8a20cd6ba4ae18ee3478844a5b28f4c44cb5a46b
uzeyirbulentsahin <uzeyir.bulent.sahin@gmail.com>
2021-03-20 22:44:34 +0300

    error meassage

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
  import { FormGroup,FormBuilder,FormControl,Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 
 @Component({
@@ -11,7 +13,9 @@ export class ProductAddComponent implements OnInit {
 
   productAddForm:FormGroup;
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder,
+              private productService:ProductService,
+              private toastrService:ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
@@ -25,5 +29,22 @@ export class ProductAddComponent implements OnInit {
       categoryId:["",Validators.required]
     })
   }
+  add(){
+    if(this.productAddForm.valid){
+      let productModel= Object.assign({},this.productAddForm.value)
+      this.productService.add(productModel).subscribe(response=>{
+        console.log(response)
+        this.toastrService.success(response.message,"Başarılı");
+      },responseError=>{
+        console.log(responseError.error)
+        this.toastrService.error(responseError.error,"Dikkat");
+      })
+     
+    }else{
+      this.toastrService.error("Formunuz eksik!","Dikkat");
+      
+    }
+
+ }
 
 }
